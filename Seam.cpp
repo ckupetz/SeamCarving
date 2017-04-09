229d4b2a4a7eece22f083d846a740daf9b82b5a6
ckupetz <ck75@zips.uakron.edu>
2017-04-09 00:08:41 -0400

    Adding comments to main.cpp and Seam.cpp

--- Seam.cpp
+++ Seam.cpp
@@ -3,9 +3,6 @@
 #include <fstream>
 
 
-#define RESET "\033[0m"
-#define RED "\033[35m" 
-
 /*
 	Output 2d Matrix of Integers
 */
@@ -14,8 +11,7 @@ void OutputMatrix(std::vector<std::vector<int> > &matrix) {
 	std::cout << "\n\nMATRIX\n";
 	for (int i = 0; i < matrix.size(); i++) {
 		for (int j = 0; j < matrix[i].size(); j++) {
-			if (matrix[i][j] == -1) std::cout << matrix[i][j] << " ";
-			else                    std::cout << RED << matrix[i][j] << RESET << " ";
+			std::cout << matrix[i][j] << " ";
 		}
 		std::cout << "\n";
 	}
@@ -27,9 +23,11 @@ void OutputMatrix(std::vector<std::vector<int> > &matrix) {
 */
 void FillFinalMatrix(std::vector<std::vector<int> > &ValueMatrix, std::vector<std::vector<int> > &FinalMatrix) {
 		
-	bool SeamFlag = false;
+	//Boolean the indicate whether the seam to be removed has been hit
+  bool SeamFlag = false;
 
-	for (int i = 0; i < ValueMatrix.size(); i++) {
+	//Iterate through the matrices and copy everything but the '-1's to the new Matrix
+  for (int i = 0; i < ValueMatrix.size(); i++) {
 		for (int j = 0; j < ValueMatrix[i].size(); j++) {
 			if (ValueMatrix[i][j] == -1) SeamFlag = true;
 			else { FinalMatrix[i][j-SeamFlag] = ValueMatrix [i][j]; }
@@ -43,6 +41,7 @@ void FillFinalMatrix(std::vector<std::vector<int> > &ValueMatrix, std::vector<st
   Read in Array from input .pgm file
 */
 void FillValueMatrix(std::vector<std::vector<int> > &matrix, std::ifstream &input) {
+  //Read in the pgm values by iterating through the empty matrix and reading in integers
   for (int i = 0; i < matrix.size(); ++i) {
     for (int j = 0; j < matrix[i].size(); ++j) {
       int temp;
@@ -57,37 +56,47 @@ void FillValueMatrix(std::vector<std::vector<int> > &matrix, std::ifstream &inpu
 	Create Energy Matrix & Cumulative Energy Matrix from Value Matrix
 */
 void FillEnergyMatrices(std::vector<std::vector<int> > &ValueMatrix, std::vector<std::vector<int> > &EnergyMatrix, std::vector<std::vector<int> > &CumulativeMatrix) {
-	for (int i = 0; i < ValueMatrix.size(); ++i) {
+	
+  //Iterate through the Matrix
+  for (int i = 0; i < ValueMatrix.size(); ++i) {
     for (int j = 0; j < ValueMatrix[i].size(); ++j) {
        
+       //Set dimensional integers to reference
        int Y_HEIGHT = ValueMatrix.size();
        int X_WIDTH = ValueMatrix[i].size();
 
-       bool widthMin  = (j == 0);
-       bool widthMax  = (j == X_WIDTH - 1);
-       bool heightMin = (i == 0);
-       bool heightMax = (i== Y_HEIGHT - 1);
+       //Booleans to check whether the indices are on the edges of the matrix
+       bool widthMin   = (j == 0);
+       bool widthMax   = (j == X_WIDTH - 1);
+       bool heightMin  = (i == 0);
+       bool heightMax  = (i== Y_HEIGHT - 1);
 
-       int leftUp     =  INT_MAX, 
-          rightUp     =  INT_MAX,
-               Up     =  0;
+       //Setting initial values for possible seam paths
+       int leftUp      =  INT_MAX, 
+          rightUp      =  INT_MAX,
+               Up      =  0;
 
-       int Energy     = ValueMatrix[i][j];
+       //Starting Energy for matrix[i][j] is the initial entry in the Value Matrix
+       int Energy      = ValueMatrix[i][j];
 
+       //Initialize Final Energy value to 0
        int FinalEnergy = 0;
 
+       //Adding Energy values depending on whether the indices are on the edges of the matrix
        if (!widthMax)  { FinalEnergy += abs(Energy - ValueMatrix[i][j+1]); }
        if (!widthMin)  { FinalEnergy += abs(Energy - ValueMatrix[i][j-1]); }
        if (!heightMax) { FinalEnergy += abs(Energy - ValueMatrix[i+1][j]); }
        if (!heightMin) { FinalEnergy += abs(Energy - ValueMatrix[i-1][j]); }
 
+       //Setting Energy Matrix and Cumulative Energy Matrix values
        EnergyMatrix[i][j] = FinalEnergy;
        CumulativeMatrix[i][j] = FinalEnergy;
 
+       //If we aren't on the top level (for Cumulative Energy Matrix)
        if (i > 0) {
         if (!widthMin)  {  leftUp = FinalEnergy + CumulativeMatrix[i-1][j-1]; }
         if (!widthMax)  { rightUp = FinalEnergy + CumulativeMatrix[i-1][j+1]; }
-                              Up = FinalEnergy + CumulativeMatrix[i-1][j];
+                               Up = FinalEnergy + CumulativeMatrix[i-1][j];
 
         CumulativeMatrix[i][j] = std::min({leftUp, rightUp, Up});
       }
@@ -166,10 +175,10 @@ void RotateNeg90(std::vector<std::vector<int> > &ValueMatrix)
   
   std::vector<std::vector<int>> RotatedMatrix    (ValueMatrix[0].size(),  std::vector<int> (ValueMatrix.size(),   0  ));
 
-
   int X_WIDTH = ValueMatrix[0].size();
   int Y_HEIGHT = ValueMatrix.size();
   for(int i=0; i<X_WIDTH; i++) {
+    
       for(int j=0; j<Y_HEIGHT; j++) {
           RotatedMatrix[i][j] = ValueMatrix[j][X_WIDTH - 1 - i];
       }
@@ -187,13 +196,9 @@ void RemoveSeam(int &Y_HEIGHT, int &X_WIDTH, int VERTICAL_SEAMS, std::vector<std
     std::vector<std::vector<int>> FinalMatrix      (Y_HEIGHT, std::vector<int> (X_WIDTH-1,  0  ));
 
 
-    //OutputMatrix(ValueMatrix);
-
     FillEnergyMatrices (ValueMatrix, EnergyMatrix, CumulativeMatrix);
 
     FindSeam           (CumulativeMatrix, ValueMatrix, Rotated);
-
-   //OutputMatrix(ValueMatrix);
 
     FillFinalMatrix(ValueMatrix, FinalMatrix);
 
@@ -204,6 +209,7 @@ void RemoveSeam(int &Y_HEIGHT, int &X_WIDTH, int VERTICAL_SEAMS, std::vector<std
 
   }
 }
+
 
 void OutputFinalMatrix(const std::vector<std::vector<int>> &matrix, std::ofstream &output, int MAX_VALUE) {
   output << "P2\n";
--- main.cpp
+++ main.cpp
@@ -9,24 +9,23 @@
 int main(int argc, char * argv[]) {
 
 
+  //Make sure we have the right number of arguments
+  if (argc != 4) { std::cout << "Please use the following format: '[filename] [VERTICAL_SEAMS] [HORIZTONAL_SEAMS]'\n"; return 0; }
 
   //Initialize our width/height variables, as well as the max B/W value (0-255)
   int MAX_VALUE, X_WIDTH, Y_HEIGHT;
   int VERTICAL_SEAMS =   atoi(argv[2]);
   int HORIZTONAL_SEAMS = atoi(argv[3]);
 
-
-  //Make sure we have the right number of arguments
-  if (argc != 4) { std::cout << "Please use the following format: '[filename] [VERTICAL_SEAMS] [HORIZTONAL_SEAMS]'\n"; return 0; }
-
   //Open the file and load it into a string
   std::ifstream input(argv[1]);
   std::string filename = argv[1];
 
-
+  //Get the literal "P2" from the .pgm file
   std::string P2_Check;
   std::getline(input, P2_Check);
 
+  //Skip the commented line in the .pgm file
   input.ignore(500, '\n');
 
   input >> X_WIDTH >> Y_HEIGHT;
@@ -48,12 +47,8 @@ int main(int argc, char * argv[]) {
 
   RotateNeg90        (ValueMatrix);
 
-  //OutputMatrix       (ValueMatrix);
 
-  
-  /*
 
-  */
   std::size_t pos = filename.find_last_of(".");
   if (pos != std::string::npos) filename = filename.substr(0, pos) + "_processed" + filename.substr(pos, pos + 2);
   //Create the outfile and fill it with the decompressed string
